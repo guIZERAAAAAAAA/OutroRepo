b33f66df1cf6c68eed08c90609cdddad3a3b69ce
Guilherme BR <guilhermedamaia64@gmail.com>
2026-03-23 21:57:04 -0300

    revisao-prova

---
+++ scripts/exercicios.js
@@ -0,0 +1,50 @@
+
+
+//exemplo 1 -----------
+
+// funcition 
+// getelement                 
+//addeventlistner
+                                    //     ("essa estrutur normalmente é a mais dificil ")
+
+
+function exemplo1() {
+    alert("oie")
+}
+// getelement
+const batata = document.getElementById("exercicio")
+//addeventlistner
+batata.addEventListener('click', () => { exemplo1() })
+
+//exemplo 2 -----------
+
+function exemplo2() {
+    alert("oie")
+}
+// getelement
+const buttonbatata = document.getElementById("batata")
+//addeventlistner
+buttonbatata.addEventListener('click', () => {exemplo2() })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
